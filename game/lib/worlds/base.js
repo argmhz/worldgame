89485f4e6b9a9fcfedee08fc12f0534512cefc9e
Marcus Daniel Hertz <arg_marcus@yahoo.dk>
2022-04-01 21:30:48 +0200

    world generator

--- game/lib/worlds/base.js
+++ game/lib/worlds/base.js
@@ -1,14 +1,13 @@
 class Base {
 
-  constructor({x,y}){
-    console.dir(x,y);
+  constructor(x,y){
     this.weather = {};
     this.animals = [];
     this.pos = {x:x,y:y};
   }
 
   step(Game){
-    console.log("do hej something")
+
   }
 
 }
